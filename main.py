9333ed6d78f4572df0d2344d6823dead6c7927ce
IndraP24 <indrashis985@gmail.com>
2021-06-17 23:15:54 +0530

    Convert root def to async def

--- main.py
+++ main.py
@@ -11,8 +11,8 @@ class Item(BaseModel):
     is_offer: Optional[bool] = None
 
 @app.get("/")
-def read_root():
-    return {"Hello": "World"}
+async def read_root():
+    return {"message": "Hello, World"}
 
 
 @app.get("/items/{item_id}")
