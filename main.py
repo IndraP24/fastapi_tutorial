24568490bd5977c6071cc270e87f5608a8119ccf
IndraP24 <indrashis985@gmail.com>
2021-06-17 23:20:48 +0530

    Edited root get operation

--- main.py
+++ main.py
@@ -4,17 +4,17 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 app = FastAPI()
 
-
+'''
 class Item(BaseModel):
     name: str
     price: float
     is_offer: Optional[bool] = None
-
+'''
 @app.get("/")
 async def read_root():
     return {"message": "Hello, World"}
 
-
+'''
 @app.get("/items/{item_id}")
 def read_item(item_id: int, q: Optional[str] = None):
     return {"item_id": item_id, "q": q}
@@ -23,3 +23,4 @@ def read_item(item_id: int, q: Optional[str] = None):
 @app.put("/items/{item_id}")
 def update_item(item_id: int, item: Item):
     return {"item_name": item.name, "item_id": item_id}
+'''
